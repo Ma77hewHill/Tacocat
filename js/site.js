2aacefd1b5e2d5581ec587a4d6eccf407c716543
Ma77hewHill <61993268+Ma77hewHill@users.noreply.github.com>
2023-01-27 14:27:02 +0000

    complete

--- js/site.js
+++ js/site.js
@@ -1,4 +1,47 @@
-//Call Hello World 
-function HelloWorld(){
-    alert("Hello World");
+//get our user input
+function getValue(){
+    //make sure the alert is invisible 
+    document.getElementById("alert").classList.remove("invisible");
+    //get user string from the page 
+    let userString = document.getElementById("userString").value;
+
+    //check for a palindrome 
+    let returnObj = checkForPaindrome(userString);
+
+    //display our message 
+    displayMessage(returnObj);
+}
+
+//check for a palindrome 
+function checkForPaindrome(userString){
+    //convert to lowercase 
+    userString = userString.toLowerCase();
+    //remove spaces and special characters
+    let regex = /[^a-z0-9]/gi;
+    userString = userString.replace(regex, "");
+
+    let revString = [];
+    let returnObj = {};
+
+    for (let index = userString.length - 1; index >= 0; index--) {
+        revString += userString[index];
+        }
+        if (revString == userString) {
+            returnObj.msg = "Well done! You entered a palindrome!"
+        }
+        else{
+            returnObj.msg = "Sorry, this is not a palindrome!"
+        }
+
+        returnObj.reversed = revString;
+
+        return returnObj;
+}
+
+//display a message to the string
+function displayMessage(returnObj){
+
+    document.getElementById("alertHeader").innerHTML = returnObj.msg;
+    document.getElementById("msg").innerHTML = `Your phrase reversed is: ${returnObj.reversed} `;
+    document.getElementById("alert").classList.remove("invisible");
 }
